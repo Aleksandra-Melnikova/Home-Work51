1cfece08840eb1192225be86b9d1a2209f755c42
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-11 20:12:59 +0600

    Create useState for numbers and this change

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 
 import './App.css'
 import Circle from "./components/Circle/circle.tsx";
+import {useState} from "react";
+
 
 const App = () => {
+const [circleItem, setCircleItem] = useState<number[]>([5, 11, 16, 23, 32]);
 
   return (
-    <>
-      <Circle numberOfCircle={1} />
-      <Circle numberOfCircle={2} />
-      <Circle numberOfCircle={3} />
-      <Circle numberOfCircle={4} />
-      <Circle numberOfCircle={5} />
-    </>
+      <>
+        <button className="btn" type="button">New numbers</button>
+        <div className="circles">
+          <Circle numberOfCircle={circleItem[0]}/>
+          <Circle numberOfCircle={circleItem[1]}/>
+          <Circle numberOfCircle={circleItem[2]}/>
+          <Circle numberOfCircle={circleItem[3]}/>
+          <Circle numberOfCircle={circleItem[4]}/>
+        </div>
+      </>
   )
 };
 
