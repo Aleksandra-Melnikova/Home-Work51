bcc169aa85d2a3bdaebbdf619264ca3bd030e280
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-11 19:36:48 +0600

    Create component circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import Circle from "./components/Circle/circle.tsx";
 
 const App = () => {
 
   return (
     <>
-
+      <Circle numberOfCircle={1} />
+      <Circle numberOfCircle={2} />
+      <Circle numberOfCircle={3} />
+      <Circle numberOfCircle={4} />
+      <Circle numberOfCircle={5} />
     </>
   )
 };
---
+++ src/components/Circle/circle.tsx
@@ -0,0 +1,16 @@
+import './circle.css';
+import * as React from "react";
+
+interface Props {
+    numberOfCircle: number;
+}
+
+const Circle: React.FC<Props> = ({numberOfCircle}) => {
+    return (
+        <div className="circle">
+            {numberOfCircle}
+        </div>
+    );
+};
+
+export default Circle;
